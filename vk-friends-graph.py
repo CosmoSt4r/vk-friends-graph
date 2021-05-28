e07f07614dc4f967a633faef236d94e757b600e9
Andrey <cosmostar@protonmail.com>
2021-05-28 17:41:34 +0500

    Comments added

--- vk-friends-graph.py
+++ vk-friends-graph.py
@@ -2,7 +2,13 @@ import requests
 import os
 import time
 
-def get_user_info(user_id, fields, token, api_version):
+def get_user_info(user_id, fields, token, api_version) -> dict:
+  '''
+  Получить информацию о человеке по его user_id
+  получает <- ID человека, поля (имя, город и т.д.), токен, версию API
+  возвращает -> API ответ (JSON) в формате словаря
+  '''
+
   params = {'user_id' : int(user_id),
               'fields' : ','.join(fields),
               'name_case' : 'Nom',
@@ -18,7 +24,12 @@ def get_user_info(user_id, fields, token, api_version):
   
   return user
 
-def make_dict_from_user_info(user, user_type):
+def make_dict_from_user_info(user, user_type) -> dict:
+  '''
+  Обработать API ответ от сервера для удобного отображения
+  получает <- словарь (API ответ), тип пользователя (я, друг, друг друга)
+  возвращает ->  обработанный словарь с информацией о человеке
+  '''
 
   user_info = {}
 
@@ -36,20 +47,35 @@ def make_dict_from_user_info(user, user_type):
 
   return user_info
 
-def make_node_from_user_info(user_info):
+def make_node_from_user_info(user_info) -> str:
+  # Преобразовать словарь с информацией о человеке в вершину графа
+
   new_node = []
   for key in user_info:
     new_node.append(user_info[key])
   new_node = ','.join(new_node)
+
   return new_node
 
-def write_to_csv(filename, fields, rows):
+def write_to_csv(filename, fields, rows) -> None:
+  '''
+  Записать информацию в csv файл
+  получает <- имя файла, поля (id, label и т.д.), строки (информация)
+  возвращает -> None
+  '''
+
   with open(filename, 'w', encoding='utf-8') as file:
     file.write(fields + '\n')
     for row in rows:
       file.write(row + '\n')
 
-def request_friends(user_id, count, fields, token, api_version):
+def request_friends(user_id, count, fields, token, api_version) -> dict:
+  '''
+  Получить список друзей человека по его user_id
+  получает <- ID человека, количество друзей, поля (имя, город и т.д.), токен, версию API
+  возвращает -> API ответ (JSON) в формате словаря
+  '''
+
   url = f'https://api.vk.com/method/friends.get'
   params = {'v' : api_version,
             'access_token' : token,
@@ -58,26 +84,35 @@ def request_friends(user_id, count, fields, token, api_version):
             'fields' : ','.join(fields)}
   return requests.get(url, params=params).json()['response']
 
-def is_mutual(friend, my_friends):
+def is_mutual(friend, my_friends) -> bool:
+  '''
+  Проверить, есть ли человек в списке друзей
+  получает <- человека для проверки, список друзей
+  возвращает -> True, если человек есть в списке друзей, False если нет
+  '''
+
   for person in my_friends:
     if person['id'] == friend['id']:
       return True
   return False
 
+# Инициализация токена
 token = os.environ.get('TOKEN')
 if not token:
   print('Токен не найден в переменных окружения')
   token = input('Введите токен: ')
 
+# Версия API и запрашиваемые поля
 version = '5.131'
 fields = ['sex', 'bdate', 'city', 'country', 'domain']
 
+# Инициализация человека для которого будет строиться граф
 while True:
   user_id = input('Введите цифровой ID: ')
-
   print('Получение информации об аккаунте')
 
   try: 
+    # Получить информацию о человеке по ID
     user = get_user_info(user_id, fields, token, version)
     user = make_dict_from_user_info(user, 'me')
     break
@@ -96,47 +131,59 @@ while True:
   except:
     print('Ошибка ввода, повторите.')
 
+# Запрос списка друзей 
 print('Получение списка друзей')
-
 my_friends = request_friends(user_id, count, fields, token, version)['items']
-
 print('Количество друзей:', len(my_friends))
 
 drop_without_mutuals = True if \
   input('Пропускать людей без общих с вами друзей? (y/n): ').lower() == 'y' else False
 dropped = 0
 
+# Инициализация списков вершин и рёбер
 edges, nodes = [], []
+# Добавить первого человека в граф
 nodes.append(make_node_from_user_info(user))
 
+# Инициализация списка друзей друзей
 friends_of_friends = []
 open_accounts = 0
 
+# Для каждого друга получить список его друзей
 for i, friend in enumerate(my_friends):
   mutual_friends = 1
   try:
+      # Запрос в API
       request = request_friends(friend.get('id'), count, fields, token, version)['items']
   except:
       continue
   
+  # Если успешно, аккаунт не закрыт
   open_accounts += 1
   
+  # Подсчет общих друзей
   for _friend in request:
     if is_mutual(_friend, my_friends):
       mutual_friends += 1
 
+  # Пропустить человека, если нет общих друзей и drop_without_mutuals=True
   if drop_without_mutuals and mutual_friends < 2:
     dropped += 1
     time.sleep(0.5)
     continue
 
+  # Добавить ребра от друга до каждого его друга
   for _friend in request:
     friends_of_friends.append(_friend)
     edges.append(f"{friend['id']},{_friend['id']},1")
 
+  # Добавить ребро до друга
   edges.append(f"{user_id},{friend['id']},{mutual_friends}")
+
+  # Добавить друга в списко вершин
   friend = make_dict_from_user_info(friend, 'friend')
   nodes.append(make_node_from_user_info(friend))
+
   print(f'Получение списка друзей друзей: {i+1}/{len(my_friends)}', end='\r')
   time.sleep(0.5)
 
@@ -144,6 +191,7 @@ print('\nЗакрытых аккаунтов среди друзей:', len(my_f
 if drop_without_mutuals:
   print('Пропущено друзей:', dropped)
 
+# Добавить вершины для друзей друзей
 for friend in friends_of_friends:
   friend = make_dict_from_user_info(friend, 'friend_of_friend')
   new_node = make_node_from_user_info(friend)
